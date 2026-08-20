3877299ed859512265572dbdb97006309468c650
angelinka66617-maker <angelinka66617@gmail.com>
2026-08-20 18:50:39 +0300

    lessons6

---
+++ src/main/java/lesson_6/Student.java
@@ -0,0 +1,45 @@
+package lesson_6;
+
+
+import java.util.*;
+
+public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private List<Integer> grades;
+
+    public Student(String name, String group, int course, List<Integer> grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = new ArrayList<>(grades);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getAverageGrade() {
+        if (grades == null || grades.isEmpty()) {
+            return 0.0;
+        }
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+}
